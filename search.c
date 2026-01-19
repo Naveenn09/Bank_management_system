a56f5cf8b421cce2d8acd32769c4900e7f51b50e
Naveenn09 <g.naveenn09@gmail.com>
2026-01-19 22:50:55 +0530

    Clean up search_account function code
    Removed commented-out code for search_account function.

--- search.c
+++ search.c
@@ -1,22 +1,3 @@
-/*#include "bank.h"
-
-void search_account(void)
-{
-    uint32_t acc_no;
-    printf("Enter Account Number: ");
-    scanf("%u", &acc_no);
-
-    account_t *acc = find_account(acc_no);
-    if (!acc) {
-        printf("Account not found\n");
-        return;
-    }
-
-    printf("AccNo:%u Name:%s Balance:%.2f Phone:%s\n",
-           acc->acc_no, acc->name, acc->balance, acc->phone);
-}
-*/
-
 #include "bank.h"
 
 void search_account(void)
